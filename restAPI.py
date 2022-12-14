4db36ef32f02d9fe9637376e0ba02fc93aea7ee6
David Rocek <rocek.david@email.cz>
2022-12-14 12:41:48 +0100

    init

---
+++ restAPI.py
@@ -0,0 +1,13 @@
+from flask import Flask, json
+
+def get_values():
+    return [{"id": 1, "message": "hello there"}]
+
+api = Flask(__name__)
+
+@api.route('/companies', methods=['GET'])
+def get_companies():
+    return json.dumps(get_values())
+
+if __name__ == "__main__":
+    api.run()
